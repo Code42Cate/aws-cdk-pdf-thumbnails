99e5e6459f78670b8842aa3e8064650593fba5e9
Jonas Scholz <jonas.scholz@bbscholz.de>
2023-01-23 13:43:32 +0100

    Initial commit

---
+++ test/aws-thumbnail.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsThumbnail from '../lib/aws-thumbnail-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-thumbnail-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsThumbnail.AwsThumbnailStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
